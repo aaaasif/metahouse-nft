e4aacb12902944dc98f4ab9d4c7b5e0995e46d1a
Arunram07 <arunram@zevocorp.in>
2022-01-28 16:32:42 +0530

    choke : update stake methods

--- src/pages/Stake/DoStake.tsx
+++ src/pages/Stake/DoStake.tsx
@@ -1,15 +1,23 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Button, Info } from "../../components";
-import { HPTBalance } from "../../utils/stake";
+import { approveHPT, approvesHPT, handleGetInitialData } from "../../utils/stake";
 
-let isApproved = false;
+interface InitialProps {
+  stakedBalance: string;
+  unstakedBalance: string;
+  stakeApproved: boolean;
+  unstakeApproved: boolean;
+  Apy: string;
+}
 
 const DoStake: React.FC = () => {
   const [isStake, setIsStake] = useState(true);
+  const [initialState, setInitialState] = useState<InitialProps | null>(null);
 
   const handleGetData = useCallback(async () => {
     try {
-      console.log(await HPTBalance());
+      const data = await handleGetInitialData();
+      if (data) setInitialState(data);
     } catch (error) {
       console.log(error);
     }
@@ -17,14 +25,26 @@ const DoStake: React.FC = () => {
 
   useEffect(() => {
     handleGetData();
-  }, []);
+  }, [handleGetData]);
+
+  const handleApprove = async (stake: boolean) => {
+    try {
+      if (stake) {
+        await approveHPT();
+      } else {
+        await approvesHPT();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const renderStake = (
     <div className="form_wrapper">
-      {isApproved ? (
+      {!initialState?.stakeApproved ? (
         <div>
           <span>First time staking OHM? Please approve (A Dao) to use your OHM for staking.</span>
-          <Button>Approve</Button>
+          <Button onClick={() => handleApprove(true)}>Approve</Button>
         </div>
       ) : (
         <div>
@@ -40,10 +60,10 @@ const DoStake: React.FC = () => {
 
   const renderUnstake = (
     <div className="form_wrapper">
-      {isApproved ? (
+      {!initialState?.unstakeApproved ? (
         <div>
           <span>First time staking OHM? Please approve (A Dao) to use your OHM for staking.</span>
-          <Button>Approve</Button>
+          <Button onClick={() => handleApprove(false)}>Approve</Button>
         </div>
       ) : (
         <div>
@@ -67,39 +87,43 @@ const DoStake: React.FC = () => {
           Unstake
         </p>
       </div>
-      {isStake ? renderStake : renderUnstake}
-      <div className="stake_content_wrapper">
-        <div className="stake_content_wrapper-switch">
-          <span>sOHM</span>
-          <p>
-            <span>gOHM</span> <Info />
-          </p>
-        </div>
-        <div className="stake_content_wrapper-block_one">
-          <div>
-            <b>Unstaked Balance</b>
-            <b>0.0000 OHM</b>
+      {initialState ? (
+        <>
+          {isStake ? renderStake : renderUnstake}
+          <div className="stake_content_wrapper">
+            <div className="stake_content_wrapper-switch">
+              <span>sOHM</span>
+              <p>
+                <span>gOHM</span> <Info />
+              </p>
+            </div>
+            <div className="stake_content_wrapper-block_one">
+              <div>
+                <b>Unstaked Balance</b>
+                <b>0.0000 OHM</b>
+              </div>
+              <div>
+                <b>Total Staked Balance</b>
+                <b>0.0000 OHM</b>
+              </div>
+            </div>
+            <div className="stake_content_wrapper-block_two">
+              <div>
+                <b>Next Reward Amount</b>
+                <b>0.0000 sOHM</b>
+              </div>
+              <div>
+                <b>Next Reward Yield</b>
+                <b>0.0000 sOHM</b>
+              </div>
+              <div>
+                <b>ROI (5-Day Rate)</b>
+                <b>0.0000 sOHM</b>
+              </div>
+            </div>
           </div>
-          <div>
-            <b>Total Staked Balance</b>
-            <b>0.0000 OHM</b>
-          </div>
-        </div>
-        <div className="stake_content_wrapper-block_two">
-          <div>
-            <b>Next Reward Amount</b>
-            <b>0.0000 sOHM</b>
-          </div>
-          <div>
-            <b>Next Reward Yield</b>
-            <b>0.0000 sOHM</b>
-          </div>
-          <div>
-            <b>ROI (5-Day Rate)</b>
-            <b>0.0000 sOHM</b>
-          </div>
-        </div>
-      </div>
+        </>
+      ) : null}
     </div>
   );
 };
--- src/utils/stake.ts
+++ src/utils/stake.ts
@@ -16,7 +16,7 @@ const HPT_Contract = new web3.eth.Contract(HPT as AbiItem[], HPT_Address);
 const sHPT_Contract = new web3.eth.Contract(sHPT as AbiItem[], sHPT_Address);
 const HPTStaking_Contract = new web3.eth.Contract(HPTStaking as AbiItem[], HPTStaking_Address);
 
-export const HPTBalance = async () => {
+const HPTBalance = async () => {
   return Web3.utils.fromWei(await HPT_Contract.methods.balanceOf(ethereum.selectedAddress).call());
 };
 
@@ -24,23 +24,23 @@ const sHPTBalance = async () => {
   return Web3.utils.fromWei(await sHPT_Contract.methods.balanceOf(ethereum.selectedAddress).call());
 };
 
-const approveHPT = async () => {
+export const approveHPT = async () => {
   const balance = await HPT_Contract.methods.balanceOf(ethereum.selectedAddress).call();
   return await HPT_Contract.methods.approve(HPTStaking_Address, balance);
 };
 
-const approvesHPT = async () => {
+export const approvesHPT = async () => {
   const balance = await sHPT_Contract.methods.balanceOf(ethereum.selectedAddress).call();
   return await sHPT_Contract.methods.approve(HPTStaking_Address, balance);
 };
 
-const stake = async (_amount) => {
+export const stake = async (_amount) => {
   const amount = Web3.utils.toWei(_amount).toString();
 
   return await HPTStaking_Contract.methods.stake(amount).send({ from: ethereum.selectedAddress });
 };
 
-const unStake = async (_amount) => {
+export const unStake = async (_amount) => {
   const amount = Web3.utils.toWei(_amount).toString();
 
   return await HPTStaking_Contract.methods
@@ -49,24 +49,72 @@ const unStake = async (_amount) => {
 };
 
 const APY = async () => {
-  return await HPTStaking_Contract.methods.FixedAPY().call();
+  return String(await HPTStaking_Contract.methods.FixedAPY().call());
+};
+//
+// const stakedAmount = async () => {
+//   const info = await HPTStaking_Contract.methods.info(ethereum.selectedAddress).call();
+//   return info[0];
+// };
+
+// const pendingRewards = async () => {
+//   const pending = await HPTStaking_Contract.methods.pendingRewards(ethereum.selectedAddress).call();
+// };
+
+// const claim = async () => {
+//   return await HPTStaking_Contract.methods
+//     .claim(ethereum.selectedAddress)
+//     .send({ from: ethereum.selectedAddress });
+// };
+
+// const totalSupply = async () => {
+//   return await HPT_Contract.methods.totalSupply().call();
+// };
+
+const allowedHPT = async () => {
+  const allowance = Number(
+    Web3.utils.fromWei(
+      await HPT_Contract.methods.allowance(ethereum.selectedAddress, HPTStaking_Address).call()
+    )
+  );
+
+  if (allowance > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const stakedAmount = async () => {
-  const info = await HPTStaking_Contract.methods.info(ethereum.selectedAddress).call();
-  return info[0];
+const allowedsHPT = async () => {
+  const allowance = Number(
+    Web3.utils.fromWei(
+      await sHPT_Contract.methods.allowance(ethereum.selectedAddress, HPTStaking_Address).call()
+    )
+  );
+
+  if (allowance > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const pendingRewards = async () => {
-  const pending = await HPTStaking_Contract.methods.pendingRewards(ethereum.selectedAddress).call();
-};
+export const handleGetInitialData = async () => {
+  try {
+    const stakedBalance = await HPTBalance();
+    const unstakedBalance = await sHPTBalance();
+    const stakeApproved = await allowedHPT();
+    const unstakeApproved = await allowedsHPT();
+    const Apy = await APY();
 
-const claim = async () => {
-  return await HPTStaking_Contract.methods
-    .claim(ethereum.selectedAddress)
-    .send({ from: ethereum.selectedAddress });
-};
-
-const totalSupply = async () => {
-  return await HPT_Contract.methods.totalSupply().call();
+    return {
+      stakedBalance,
+      unstakedBalance,
+      stakeApproved,
+      unstakeApproved,
+      Apy,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
