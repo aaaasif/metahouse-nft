a364b6d7aeed9243a505b033236e1c697cea6a0a
Braj1410 <braj1410@gmail.com>
2022-02-01 17:07:32 +0530

    fix : modal

--- src/pages/Stake/DoStake.tsx
+++ src/pages/Stake/DoStake.tsx
@@ -53,7 +53,6 @@ const DoStake: React.FC = () => {
       console.log(data);
       if (data) {
         setInitialState(data);
-        if (data.unstakedBalance <= 0 && data.stakedBalance <= 0) setModal("stake");
       }
     } catch (error) {
       console.log(error);
@@ -87,7 +86,13 @@ const DoStake: React.FC = () => {
     }
     setLoading(true);
     try {
-      await unStake(String(withdraw));
+      const data = await unStake(String(withdraw));
+
+      if (data?.error === true) {
+        setModal("unstakeApprove");
+        return;
+      }
+
       window.location.reload();
     } catch (error) {
       console.log(error);
@@ -97,6 +102,10 @@ const DoStake: React.FC = () => {
   };
 
   const handleDeposit = async () => {
+    if (initialState && initialState?.unstakedBalance <= 0) {
+      setModal("stake");
+      return;
+    }
     setLoading(true);
     try {
       await stake(String(deposit));
@@ -125,7 +134,8 @@ const DoStake: React.FC = () => {
       {!initialState?.stakeApproved ? (
         <div>
           <span style={{ textAlign: "center" }}>
-            First time staking HPT? Please approve (A Dao) to use your HPT for staking.
+            First time staking HPT? Please approve (A Dao) to use your HPT for
+            staking.
           </span>
           <Button disabled={loading} onClick={() => handleApprove(true)}>
             Approve
@@ -139,11 +149,18 @@ const DoStake: React.FC = () => {
               value={deposit === 0 ? "" : deposit}
               onChange={(e) => setDeposit(Number(e.target.value))}
             />
-            <button onClick={() => setDeposit(Math.floor(initialState.unstakedBalance))}>
+            <button
+              onClick={() =>
+                setDeposit(Math.floor(initialState.unstakedBalance))
+              }
+            >
               Max
             </button>
           </div>
-          <Button disabled={loading} onClick={() => handleDeposit()}>
+          <Button
+            disabled={loading || !deposit}
+            onClick={() => handleDeposit()}
+          >
             Stake
           </Button>
         </div>
@@ -156,7 +173,8 @@ const DoStake: React.FC = () => {
       {!initialState?.unstakeApproved ? (
         <div>
           <span style={{ textAlign: "center" }}>
-            First time unstaking HPT? Please approve (A Dao) to use your sHPT for unstaking.
+            First time unstaking HPT? Please approve (A Dao) to use your sHPT
+            for unstaking.
           </span>
           <Button disabled={loading} onClick={() => handleApprove(false)}>
             Approve
@@ -175,7 +193,9 @@ const DoStake: React.FC = () => {
             />
             <button
               onClick={() => {
-                let value = Math.floor(initialState.stakedBalance) > initialState.unstakeLimit;
+                let value =
+                  Math.floor(initialState.stakedBalance) >
+                  initialState.unstakeLimit;
                 let withdrawAmt = value
                   ? initialState.unstakeLimit
                   : Math.floor(initialState.stakedBalance);
@@ -186,9 +206,11 @@ const DoStake: React.FC = () => {
             </button>
           </div>
           <Button
-            disabled={loading || unstakeMaxDisable}
+            disabled={loading || unstakeMaxDisable || !withdraw}
             onMouseEnter={() =>
-              withdraw > initialState.unstakeLimit ? setToaster(true) : undefined
+              withdraw > initialState.unstakeLimit
+                ? setToaster(true)
+                : undefined
             }
             onMouseLeave={() => setToaster(false)}
             onClick={() => handleWithdraw()}
@@ -231,13 +253,19 @@ const DoStake: React.FC = () => {
                 <div>
                   <b>Unstaked Balance</b>
                   <b>
-                    {new Intl.NumberFormat("en-US", {}).format(initialState?.unstakedBalance)} HPT
+                    {new Intl.NumberFormat("en-US", {}).format(
+                      initialState?.unstakedBalance
+                    )}{" "}
+                    HPT
                   </b>
                 </div>
                 <div>
                   <b>Total Staked Balance</b>
                   <b>
-                    {new Intl.NumberFormat("en-US", {}).format(initialState?.stakedBalance)} sHPT
+                    {new Intl.NumberFormat("en-US", {}).format(
+                      initialState?.stakedBalance
+                    )}{" "}
+                    sHPT
                   </b>
                 </div>
               </div>
@@ -275,13 +303,19 @@ const DoStake: React.FC = () => {
           </div>
         )}
       </div>
-      <div className={loading ? "transaction_loader active" : "transaction_loader"}>
+      <div
+        className={loading ? "transaction_loader active" : "transaction_loader"}
+      >
         <div className="loader">
           <img src={loader} alt="loader" />
           <p>Don't refresh your page</p>
         </div>
       </div>
-      <div className={modal === "stake" ? "transaction_loader active" : "transaction_loader"}>
+      <div
+        className={
+          modal === "stake" ? "transaction_loader active" : "transaction_loader"
+        }
+      >
         <div className="loader">
           <div
             style={{
@@ -301,7 +335,11 @@ const DoStake: React.FC = () => {
         </div>
       </div>
       <div
-        className={modal === "unstake" ? "transaction_loader active" : "transaction_loader"}
+        className={
+          modal === "unstake"
+            ? "transaction_loader active"
+            : "transaction_loader"
+        }
         onClick={() => setModal(null)}
       >
         <div className="loader">
@@ -318,6 +356,30 @@ const DoStake: React.FC = () => {
             <Link to="/stake?method=stake">
               <Button>Stake HPT</Button>
             </Link>
+          </div>
+        </div>
+      </div>
+      <div
+        className={
+          modal === "unstakeApprove"
+            ? "transaction_loader active"
+            : "transaction_loader"
+        }
+      >
+        <div className="loader">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              justifyContent: "center",
+              gridGap: 10,
+            }}
+          >
+            <p>Allowance is low,approve tokens to proceed</p>
+            <Button disabled={loading} onClick={() => handleApprove(false)}>
+              Approve
+            </Button>
           </div>
         </div>
       </div>
--- src/utils/stake.ts
+++ src/utils/stake.ts
@@ -14,25 +14,36 @@ const web3 = new Web3(ethereum);
 
 const HPT_Contract = new web3.eth.Contract(HPT as AbiItem[], HPT_Address);
 const sHPT_Contract = new web3.eth.Contract(sHPT as AbiItem[], sHPT_Address);
-const HPTStaking_Contract = new web3.eth.Contract(HPTStaking as AbiItem[], HPTStaking_Address);
+const HPTStaking_Contract = new web3.eth.Contract(
+  HPTStaking as AbiItem[],
+  HPTStaking_Address
+);
 
 const HPTBalance = async () => {
-  return Web3.utils.fromWei(await HPT_Contract.methods.balanceOf(ethereum.selectedAddress).call());
+  return Web3.utils.fromWei(
+    await HPT_Contract.methods.balanceOf(ethereum.selectedAddress).call()
+  );
 };
 
 const sHPTBalance = async () => {
-  return Web3.utils.fromWei(await sHPT_Contract.methods.balanceOf(ethereum.selectedAddress).call());
+  return Web3.utils.fromWei(
+    await sHPT_Contract.methods.balanceOf(ethereum.selectedAddress).call()
+  );
 };
 
 export const approveHPT = async () => {
-  const balance = await HPT_Contract.methods.balanceOf(ethereum.selectedAddress).call();
+  const balance = await HPT_Contract.methods
+    .balanceOf(ethereum.selectedAddress)
+    .call();
   return await HPT_Contract.methods
     .approve(HPTStaking_Address, balance)
     .send({ from: ethereum.selectedAddress });
 };
 
 export const approvesHPT = async () => {
-  const balance = await sHPT_Contract.methods.balanceOf(ethereum.selectedAddress).call();
+  const balance = await sHPT_Contract.methods
+    .balanceOf(ethereum.selectedAddress)
+    .call();
   return await sHPT_Contract.methods
     .approve(HPTStaking_Address, balance)
     .send({ from: ethereum.selectedAddress });
@@ -41,33 +52,69 @@ export const approvesHPT = async () => {
 export const stake = async (_amount: string) => {
   const amount = Web3.utils.toWei(_amount).toString();
 
-  const allowance = Web3.utils.toBN(
-    Web3.utils.fromWei(
-      await HPT_Contract.methods.allowance(ethereum.selectedAddress, HPTStaking_Address).call()
-    )
+  const allowance = Web3.utils.fromWei(
+    await HPT_Contract.methods
+      .allowance(ethereum.selectedAddress, HPTStaking_Address)
+      .call()
   );
 
-  if (allowance >= Web3.utils.toBN(_amount)) {
-    console.log(allowance.toString(), _amount);
-    return await HPTStaking_Contract.methods.stake(amount).send({ from: ethereum.selectedAddress });
+  if (Number(allowance) >= Number(_amount)) {
+    return await HPTStaking_Contract.methods
+      .stake(amount)
+      .send({ from: ethereum.selectedAddress });
   } else {
-    console.log("Approve balance to stake");
+    console.log("Allowance is low,approve tokens to proceed");
   }
 };
 
 export const unStake = async (_amount: string) => {
-  console.log(_amount);
   const amount = Web3.utils.toWei(_amount).toString();
 
-  return await HPTStaking_Contract.methods.unstake(amount).send({ from: ethereum.selectedAddress });
+  const allowance = Web3.utils.fromWei(
+    await sHPT_Contract.methods
+      .allowance(ethereum.selectedAddress, HPTStaking_Address)
+      .call()
+  );
+
+  if (Number(allowance) >= Number(_amount)) {
+    return await HPTStaking_Contract.methods
+      .unstake(amount)
+      .send({ from: ethereum.selectedAddress });
+  } else {
+    console.log("Approve token");
+    return { error: true };
+  }
 };
+
+// export const unStake = async (_amount: string) => {
+//   const amount = Web3.utils.toWei(_amount).toString();
+
+//   const allowance = Web3.utils.toBN(
+//     Web3.utils.fromWei(
+//       await sHPT_Contract.methods
+//         .allowance(ethereum.selectedAddress, HPTStaking_Address)
+//         .call()
+//     )
+//   );
+
+//   if (allowance >= Web3.utils.toBN(_amount)) {
+//     console.log(allowance, Web3.utils.toBN(_amount));
+//     return await HPTStaking_Contract.methods
+//       .unstake(amount)
+//       .send({ from: ethereum.selectedAddress });
+//   } else {
+//     console.log("Approve balance to unstake");
+//   }
+// };
 
 const APY = async () => {
   return String(await HPTStaking_Contract.methods.FixedAPY().call());
 };
 
 export const totalAmountStaked = async () => {
-  return String(Web3.utils.fromWei(await sHPT_Contract.methods.totalSupply().call()));
+  return String(
+    Web3.utils.fromWei(await sHPT_Contract.methods.totalSupply().call())
+  );
 };
 
 // const stakedAmount = async () => {
@@ -77,7 +124,9 @@ export const totalAmountStaked = async () => {
 
 const pendingRewards = async () => {
   const pendingAmount = Web3.utils.fromWei(
-    await HPTStaking_Contract.methods.pendingRewards(ethereum.selectedAddress).call()
+    await HPTStaking_Contract.methods
+      .pendingRewards(ethereum.selectedAddress)
+      .call()
   );
   return pendingAmount;
 };
@@ -95,7 +144,9 @@ export const claim = async () => {
 const allowedHPT = async () => {
   const allowance = Number(
     Web3.utils.fromWei(
-      await HPT_Contract.methods.allowance(ethereum.selectedAddress, HPTStaking_Address).call()
+      await HPT_Contract.methods
+        .allowance(ethereum.selectedAddress, HPTStaking_Address)
+        .call()
     )
   );
 
@@ -109,7 +160,9 @@ const allowedHPT = async () => {
 const allowedsHPT = async () => {
   const allowance = Number(
     Web3.utils.fromWei(
-      await sHPT_Contract.methods.allowance(ethereum.selectedAddress, HPTStaking_Address).call()
+      await sHPT_Contract.methods
+        .allowance(ethereum.selectedAddress, HPTStaking_Address)
+        .call()
     )
   );
 
@@ -121,7 +174,9 @@ const allowedsHPT = async () => {
 };
 
 const getUnstakeLimit = async () => {
-  return Web3.utils.fromWei(await HPTStaking_Contract.methods.UnStakeLimit().call());
+  return Web3.utils.fromWei(
+    await HPTStaking_Contract.methods.UnStakeLimit().call()
+  );
 };
 
 export const handleGetInitialData = async () => {
