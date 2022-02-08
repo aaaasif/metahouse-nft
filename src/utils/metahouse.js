f70b40d97486393c8c0a8076c260b79809366bad
ArunKutty798 <arunkutty798@gmail.com>
2022-02-08 15:09:22 +0530

    choke : abi

--- src/utils/metahouse.js
+++ src/utils/metahouse.js
@@ -2,8 +2,8 @@ import Web3 from "web3";
 import metahouseabi from "./abis/metahouse.json";
 import nftabi from "./abis/nft.json";
 
-const nftaddress = "0x474FfF0e906aD3d90831cC61491f282ab8Ae7C82";
-const metahouseaddress = "0xA4A8a73729a61338964272075f850bCB394fF374";
+const nftaddress = "0xE3775809E01c34cf5C7edc52F1aD6be6BE430Af7";
+const metahouseaddress = "0x7dFd1c3eb0896A8C9CF995ED4f52C0DCe411B933";
 
 const web3 = new Web3(window.ethereum);
 
@@ -15,12 +15,35 @@ export const stake = async (tokenid) => {
     from: "0x62562d3F2a512Dee20F13D7e43D0B4fC712CaA4A",
   });
 };
+export const stakehotel = async (tokenid) => {
+  const staking = metahouse.methods.stake("1").send({
+    from: "0x62562d3F2a512Dee20F13D7e43D0B4fC712CaA4A",
+  });
+};
 
 export const unstake = async (tokenid) => {
   const unstaking = metahouse.methods.unstake("1").send({
     from: "0x62562d3F2a512Dee20F13D7e43D0B4fC712CaA4A",
   });
 };
+export const unstakehotel = async (tokenid) => {
+  const unstaking = metahouse.methods.unstake("1").send({
+    from: "0x62562d3F2a512Dee20F13D7e43D0B4fC712CaA4A",
+  });
+};
+
 export const balance = async () => {
-  const balance = metahouse.methods.balanceOf("0x62562d3F2a512Dee20F13D7e43D0B4fC712CaA4A").call();
+  const balance = metahouse.methods
+    .balanceOf("0x62562d3F2a512Dee20F13D7e43D0B4fC712CaA4A")
+    .call();
+  return balance;
+};
+
+export const rewardcalculator = async (tokenid) => {
+  const reward = metahouse.methods.rewardcalculator(tokenid).call();
+  return reward;
+};
+export const rewardcalculatorhotel = async () => {
+  const reward = metahouse.methods.rewardcalculatorhotel(tokenid).call();
+  return reward;
 };
