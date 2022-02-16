2ea3aa2845189fd63ba96258c41f5c2dce2aa39c
ArunKutty798 <arunkutty798@gmail.com>
2022-02-16 12:49:39 +0530

    choke:metahouse.js

--- src/utils/metahouse.js
+++ src/utils/metahouse.js
@@ -110,5 +110,6 @@ export const firstepoch = async (address, tokenid) => {
   const res = await metahouse.methods.getEpoch(address, tokenid).call();
   return res[0], tokenid;
 };
+
 // Hotel id
 // 303 375 421 501 625 668 702 895 921 1015 1106 1523 1847 1980 2015 2100 2298 2341 2450 2458
